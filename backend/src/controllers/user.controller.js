b9df3999d049120f302b688548364f3de27e4a09
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-02-04 04:57:59 +0530

    login logic completed

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,12 +1,31 @@
-import {asyncHandler} from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import { apiError } from "../utils/apiError.js"
 import { User } from "../models/user.models.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { apiResponse } from "../utils/apiResponse.js";
+
+//internal method to generate refresh and access tokens
+const generateAccessAndRefreshTokens = async (userId)=>{
+    try {
+        //finding user and generation
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        //saving in db
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new apiError(500, "Something went wrong while generating tokens")
+    }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
     //getting details
-    const {name, email, password} = req.body
+    const { name, email, password } = req.body
     console.log("email:", email);
 
     //validation
@@ -17,8 +36,8 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     //checking for user existance
-    const userExistance = User.findOne({
-        $or: [{name}]
+    const userExistance = await User.findOne({
+        $or: [{ email }]
     })
     if (userExistance) {
         throw new apiError(409, "Email already exists!")
@@ -32,20 +51,19 @@ const registerUser = asyncHandler(async (req, res) => {
     // const avatar = await uploadOnCloudinary(avatarLocalPath)
 
     //user object: create entry in db
-
-    User.create({
+    const newUser = await User.create({
         name,
         email,
         password
     })
 
     //remove pass and refresh token from response
-    const creationCheck = await User.findById(user._id).select(
+    const creationCheck = await User.findById(newUser._id).select(
         "-password -refreshToken"
     )
 
     //check for user creation
-    if(!creationCheck){
+    if (!creationCheck) {
         throw new apiError(500, "something went wrong while registering the user")
     }
 
@@ -55,4 +73,54 @@ const registerUser = asyncHandler(async (req, res) => {
     )
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async (req, res) => {
+    //req body from data
+    const { email, password } = req.body
+    if (!email && !password) {
+        throw new apiError(400, "email or password is required")
+    }
+
+    //find user in db
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new apiError(404, "User does not exist. Please Register!");
+    }
+
+    //passwordcheck
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new apiError(401, "Invalid user credentials")
+    }
+
+    //access and refresh token
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    //remove password and refresh token form response
+    const loggedInUser = await User.findById(user._id)
+    .select("-password -refreshToken")
+
+    //additional security
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    //send cookie
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new apiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged in successfully"
+        )
+    )
+})
+
+export { registerUser }
