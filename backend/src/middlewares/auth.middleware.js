fdef851ce660d7d177d13085083a051abf2ae24b
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-02-04 05:39:22 +0530

    authorization middleware completed

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { apiError } from "../utils/apiError"
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.models.js"
+
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        //accessing cookies (here, access token cookie)
+        const token = req.cookies?.accessToken ||
+            //in case user send a custom header (in case of mobile devices)
+            req.header("Authorization")?.replace("Bearer ", "")
+    
+        if (!token) {
+            throw new apiError(401, "Unauthorized request")
+        }
+    
+        //verifying the token
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        //fetching the user from database
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+    
+        if (!user) {
+            throw new apiError(401, "Invalid access token")
+        }
+    
+        req.user = user;
+        next()
+
+    } catch (error) {
+        throw new apiError(401, error?.message || "Invalid access token")
+    }
+})
