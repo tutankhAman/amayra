5329d0eea1797604b97de8f9ea7c44b5b59f2e50
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-02-06 16:28:15 +0530

    delete function implemetned for deletion of images in cloudinary

---
+++ backend/src/utils/deleteFromCloudinary.js
@@ -0,0 +1,22 @@
+import {v2 as cloudinary} from "cloudinary";
+
+export const deleteFromCloudinary = async(publicId) => {
+    try {
+        if (!publicId) {
+            console.log("No public ID provided for deletion");
+            return null;
+        }
+
+        console.log("Attempting to delete file with public ID:", publicId)
+
+        const respinse = await cloudinary.uploader.destroy(publicId);
+
+        console.log("Cloudinary deletion response", response)
+
+        return response
+    } catch (error) {
+        console.error("Cloudinary deletion error:", error)
+        return null;
+    }
+};
+
