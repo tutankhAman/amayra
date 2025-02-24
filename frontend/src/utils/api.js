b9f3da631faa5964089e934713b7467ba9f3aa8a
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-02-24 17:04:13 +0530

    final touchees

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/v1';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1';
 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
-    'Content-Type': 'application/json' // Default to JSON
+    'Content-Type': 'application/json'
   },
   withCredentials: true
 });
