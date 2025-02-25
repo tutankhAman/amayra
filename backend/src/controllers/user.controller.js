eb52b8f661ab91bf7f77f3213b603826e567421e
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-02-25 10:36:47 +0530

    update cookie options in refreshAccessToken; add sameSite attribute for improved security

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -190,7 +190,8 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
  
      const options = {
          httpOnly: true,
-         secure: true
+         secure: true,
+         sameSite:"None"
      }
  
      //generating tokens again
